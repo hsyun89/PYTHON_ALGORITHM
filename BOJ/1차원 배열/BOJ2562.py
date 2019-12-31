08db7553d7e4fa7af8689294b2e9501283c662f8
HyunSu Yoon <hsyun89@naver.com>
2020-01-01 02:53:47 +0900

    .

--- BOJ/1차원 배열/BOJ2562.py
+++ BOJ/1차원 배열/BOJ2562.py
@@ -1,13 +1,21 @@
+# import sys
+
+# max = 0
+# idx = 0 
+
+# for i in range(9):
+#     a = int(sys.stdin.readline())
+#     if a > max:
+#         max = a
+#         idx = i+1
+
+# print(max)
+# print(idx)
+
 import sys
 
-max = 0
-idx = 0 
+nums = [int(sys.stdin.readline()) for i in range(9)]
 
-for i in range(9):
-    a = int(sys.stdin.readline())
-    if a > max:
-        max = a
-        idx = i+1
-
-print(max)
-print(idx)
+result = max(nums)
+print(result)
+print(nums.index(result)+1)
