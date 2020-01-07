98654efab532b22df79153f28ac214f9308a15e6
윤현수 <31728364+hsyun89@users.noreply.github.com>
2020-01-07 22:34:44 +0900

    BOJ2920

--- BOJ/1차원 배열/BOJ2920.py
+++ BOJ/1차원 배열/BOJ2920.py
@@ -1,24 +1,35 @@
+# import sys
+
+# nums = list(map(int, sys.stdin.readline().split(" ")))
+# flag = 0
+
+# if nums[0] == 1:
+#     for i in range(8):
+#         if nums[i] != i+1:
+#             flag = 1
+#     if flag == 1:
+#         print("mixed")
+#     else:
+#         print("ascending")
+# elif nums[0] ==8:
+#     for i in range(8):
+#         if nums[i] != 8-i:
+#             flag = 1
+#     if flag == 1:
+#         print("mixed")
+#     else:
+#         print("descending")
+# else:
+#     print("mixed")
+
 import sys
 
-nums = list(map(int, sys.stdin.readline().split(" ")))
-flag = 0
+# nums = list(map(int, sys.stdin.readline().split(" ")))
+nums = input()
 
-if nums[0] == 1:
-    for i in range(8):
-        if nums[i] != i+1:
-            flag = 1
-    if flag == 1:
-        print("mixed")
-    else:
-        print("ascending")
-elif nums[0] ==8:
-    for i in range(8):
-        if nums[i] != 8-i:
-            flag = 1
-    if flag == 1:
-        print("mixed")
-    else:
-        print("descending")
+if nums == [1,2,3,4,5,6,7,8]:
+	print("ascending")
+elif nums == "8 7 6 5 4 3 2 1":
+	print("descending")
 else:
-    print("mixed")
-
+	print("mixed")
