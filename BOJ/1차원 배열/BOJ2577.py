18c027de785507814214643c70506bc56fed53c1
HyunSu Yoon <hsyun89@naver.com>
2020-02-19 23:54:32 +0900

    .

--- BOJ/1차원 배열/BOJ2577.py
+++ BOJ/1차원 배열/BOJ2577.py
@@ -1,17 +1,18 @@
 import sys
 #specific한 경우
-result=1
-for i in range(3):
-    result *= int(sys.stdin.readline())
+# result=1
+# for i in range(3):
+#     result *= int(sys.stdin.readline())
 
-#제너럴한입력
-# nums = [int(sys.stdin.readline()) for i in range(3)]
+# 제너럴한입력
+nums = [int(sys.stdin.readline()) for i in range(3)]
 
-# result = 1
+result = 1
 
-# for i in range(len(nums)):
-#     result = result * nums[i]
-
+for i in range(len(nums)):
+    result = result * nums[i]
+    
+###
 result = str(result)
 
 for i in range(10):
